df79ffd23d0e309295e4570c7777b74efb73de37
Fabian Campoverde <fabi22isra@yahoo.com>
2024-12-13 09:32:51 -0500

    Nuevo commit avance en clases

--- proyectoClase/back/routes/test.routes.js
+++ proyectoClase/back/routes/test.routes.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(request, response) {
+  response.json({
+    status: true,
+    message: "TODO ESTA OK"
+  });
 });
-
 module.exports = router;
