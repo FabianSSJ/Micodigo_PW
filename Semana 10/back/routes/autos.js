3a522c7cdeeb1fdbf6d0a29f06b3c813e075cb4b
Fabian Campoverde <fabi22isra@yahoo.com>
2025-01-17 08:16:24 -0500

    Update de rutas

--- Semana 10/back/routes/autos.js
+++ Semana 10/back/routes/autos.js
@@ -1,10 +1,46 @@
 const express = require("express");
 const router = express.Router();
+
 const autoController = require("../controllers/auto.controller");
 
-router.get("/", autoController.getAutos);
-router.post("/", autoController.createAuto);
-router.put("/:id", autoController.updateAuto);
-router.delete("/:id", autoController.deleteAuto);
 
-module.exports = router; 
+router.get("/", function (request, response) {
+  const result = autoController.getAutos();
+  response.status(200).json({
+    data: result,
+    status: true,
+    message: "Lista de autos obtenida.",
+  });
+});
+
+
+router.post("/", function (request, response) {
+  const result = autoController.createAuto(request.body);
+  response.status(201).json({
+    data: result,
+    status: true,
+    message: "Auto creado satisfactoriamente.",
+  });
+});
+
+
+router.put("/:id", function (request, response) {
+  const result = autoController.updateAuto(request.params.id, request.body);
+  response.status(200).json({
+    data: result,
+    status: true,
+    message: "Auto actualizado satisfactoriamente.",
+  });
+});
+
+
+router.delete("/:id", function (request, response) {
+  const result = autoController.deleteAuto(request.params.id);
+  response.status(200).json({
+    data: result,
+    status: true,
+    message: "Auto eliminado satisfactoriamente.",
+  });
+});
+
+module.exports = router;
