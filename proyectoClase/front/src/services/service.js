6f75c202fbb8b9e3b0a9a91555a22132352807dc
Fabian Campoverde <fabi22isra@yahoo.com>
2025-02-01 20:00:16 -0500

    Update librerias

--- proyectoClase/front/src/services/service.js
+++ proyectoClase/front/src/services/service.js
@@ -1,36 +1,22 @@
+import axios from "axios";
 import configs from "../configs/env";
 
 export const Services = async (body = "", url, method = "POST") => {
-    const requestInit = {
-        method: method, 
-        headers: { "Content-Type": "application/json" },
-    };
-
-    // Agregar `body` solo si el método lo requiere (POST, PUT, DELETE)
-    if (method !== "GET") {
-        requestInit.body = JSON.stringify(body);
-    }
-
-    console.log(`${configs.server.url}${url}`, requestInit);
-
     try {
-        const response = await fetch(`${configs.server.url}${url}`, requestInit);
+        const response = await axios({
+            method: method,
+            url: `${configs.server.url}${url}`,
+            headers: { "Content-Type": "application/json" },
+            data: method !== "GET" ? body : null, 
+        });
 
-        // Verifica si la respuesta HTTP es válida
-        if (!response.ok) {
-            throw new Error(`Error HTTP: ${response.status}`);
-        }
-
-        // Verifica que la respuesta sea de tipo JSON antes de procesarla
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.includes("application/json")) {
-            const jsonResponse = await response.json();
-            return jsonResponse;
-        } else {
-            throw new Error("La respuesta no es un JSON válido");
-        }
+        console.log(`${configs.server.url}${url}`, response.config);
+        return response.data; 
     } catch (error) {
         console.error("Error en la solicitud:", error);
-        return { status: false, message: error.message || "Error desconocido" };
+        return {
+            status: false,
+            message: error.response?.data?.message || error.message || "Error desconocido",
+        };
     }
 };
